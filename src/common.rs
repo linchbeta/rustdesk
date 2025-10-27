6d5cfed11d1826738f47947b9fed3da04eb1d61d
linchbeta <linch.beta@gmail.com>
2025-10-27 23:57:15 +0800

    Update common.rs

--- src/common.rs
+++ src/common.rs
@@ -1024,7 +1024,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "http://lasx.me:21114".to_owned()
+    "https://lasx.me:21114".to_owned()
 }
 
 #[inline]
