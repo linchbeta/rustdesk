a3796efa2ac6b7c70ca7a58795706777ef003f94
linchbeta <linch.beta@gmail.com>
2025-10-28 15:54:36 +0800

    Update common.rs alow https 21114

--- src/common.rs
+++ src/common.rs
@@ -992,12 +992,12 @@ pub fn get_api_server(api: String, custom: String) -> String {
     if res.ends_with('/') {
         res.pop();
     }
-    if res.starts_with("https")
-        && res.ends_with(":21114")
-        && get_builtin_option(keys::OPTION_ALLOW_HTTPS_21114) != "N"
-    {
-        return res.replace(":21114", "");
-    }
+    //if res.starts_with("https")
+    //    && res.ends_with(":21114")
+    //    && get_builtin_option(keys::OPTION_ALLOW_HTTPS_21114) != "Y"
+    //{
+    //    return res.replace(":21114", "");
+    //}
     res
 }
 
